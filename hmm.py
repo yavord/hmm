d521ddde03edeb73dea26e3b39d1f5da7ee6d6cd
Yavor Dimitrov <yavor.d0@gmail.com>
2019-12-10 20:11:05 -0600

    .

--- hmm.py
+++ hmm.py
@@ -79,13 +79,13 @@ def forward(X,A,E):
     # Adapt the viterbi() function to account for the differences.
 
     # Middle columns
+    # for ...
     for i,s in enumerate(X):
         for l in emittingStates:
             terms = [F[k][i] * A[k][l] for k in allStates]
-            print(terms)
-            F[l][i+1] = max(terms) * E[l][s]
-    # for ...
+            F[l][i+1] = sum(terms) * E[l][s]
 
+    print(F)
     # Last columns
     # for ...:
     #     F['E'][-1] += ...
